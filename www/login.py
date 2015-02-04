74e74e15cad87a044579051be939c00a67a74572
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-02-04 20:13:34 -0200

    login

--- www/login.py
+++ www/login.py
@@ -1,15 +1,21 @@
 
-
-<html>
-<body>
-
 <%
-
 import sqlite3 as lite
 import sys
 from subprocess import call
 import os 
 import config
+from server.page import html, textStyle
+from server.managers import sessionManager
+
+html.initHTML(self)
+html.beginHead(self)
+html.setMetaUTF8(self)
+html.endHead(self)
+html.beginContent(self)
+
+
+
 
 os.chdir(config.__WWW_DIR__)
 
@@ -25,11 +31,12 @@ with con:
 
     rows = cur.fetchall()
 
-    for row in rows:
-        print row
+    if len(rows) >= 1:
+        print u"Usuário Encontrado"
+    else:
+        print u"senha inválida"
 
+html.endContent(self)
+html.endHtml(self)
 %>
-
-</body>
-</html>
 
