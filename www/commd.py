b6d0bf00d2a57eadb9999eb50d2fa4a608d7b9d1
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-03-12 14:33:21 -0300

    Try no osciloscopio

--- www/commd.py
+++ www/commd.py
@@ -1,11 +1,26 @@
 <%
+from server.page import html, textStyle
+from server.managers import sessionManager
+
+html.initHTML(self)
+html.beginHead(self)
+html.setMetaUTF8(self)
+html.endHead(self)
+html.beginContent(self)
+
 def write(cmd):
-    import visa;
-    rm = visa.ResourceManager("@py")
-    inst = rm.open_resource("TCPIP::192.168.134.102")
-    inst.write(cmd)
+    try:
+        import visa;
+        rm = visa.ResourceManager("@py")
+        inst = rm.open_resource("TCPIP::192.168.134.102")
+        inst.write(cmd)
+    except:
+        print(u"Não encontrado")
 
 
 login = __GET__['cmd'][0]
 write(login)
+
+html.endContent(self)
+html.endHtml(self)
 %>
